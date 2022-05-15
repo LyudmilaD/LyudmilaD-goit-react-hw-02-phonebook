2f667e78f22d670e8f14a2a8ec0d2ca4fc9ee8ae
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-05-16 02:09:34 +0300

    task 2 filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,21 +1,21 @@
-// import PropTypes from 'prop-types';
-// import styles from '../Form/Form.module.css';
-// function Filter({ value, onChange }) {
-//   return (
-//     <label className={styles.formLabel}>
-//       Find contacts by name
-//       <input
-//         className={styles.input}
-//         tupe="text"
-//         value={value}
-//         onChange={onChange}
-//       />
-//     </label>
-//   );
-// }
+import PropTypes from 'prop-types';
+import styles from '../Form/Form.module.css';
+function Filter({ value, onChange }) {
+  return (
+    <label className={styles.formLabel}>
+      Find contacts by name
+      <input
+        className={styles.input}
+        tupe="text"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
 
-// Filter.propTypes = {
-//   value: PropTypes.string,
-//   onChange: PropTypes.func,
-// };
-// export default Filter;
+Filter.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+export default Filter;
