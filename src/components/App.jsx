497469891e7828012e6a16fbae1c3a4aa1e47122
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-05-16 02:08:25 +0300

    task 2 form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import Form from './Form/Form'
+import ContactList from './List/List'
+import Filter from './Filter/Filter'
+import styles from './Form/Form.module.css';
+export class App extends React.Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  }
+submitHandler = data =>{
+  const flag = this.state.contacts.find(contact => contact.name === data.name)
+  this.setState(prevState =>{
+   return  flag?alert(`${data.name} is alredy in contacts`): {contacts :[data, ...prevState.contacts]}
+    
+     
+  })
+}
+changeFilter = e=>{
+  this.setState({filter: e.currentTarget.value})
+}
+deleteContacts = id =>{
+  this.setState(prevState =>({
+    contacts: prevState.contacts.filter(contact => contact.id !== id)
+  }));
+}
+
+
+    render(){
+      const normalizedFilter= this.state.filter.toLowerCase()
+      const visibleContacts = this.state.contacts.filter(contact =>contact.name.toLowerCase().includes(normalizedFilter) )
+    return (<div className={styles.wrap}>
+      <h2>Phonebook</h2>
+      <Form onSubmit={this.submitHandler} />
+      <h2>Contacts</h2>
+      <ContactList contacts={visibleContacts} deleteContacts={this.deleteContacts}>
+      <Filter value={this.state.filter} onChange ={this.changeFilter}/>
+      </ContactList>
+    </div>)
+    
+  }
+
+}
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid';
+import styles from './Form.module.css';
+import React from 'react';
+export class Form extends React.Component {
+  state = {
+    name: '',
+    id: nanoid(),
+    number: '',
+  };
+
+  handelNameChange = e => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  handelNumberChange = e => {
+    this.setState({ number: e.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ name: '', id: '', number: '' });
+  };
+  render() {
+    return (
+      <>
+        <form className={styles.form} onSubmit={this.handleSubmit}>
+          <label className={styles.formLabel}>
+            {' '}
+            Name
+            <input
+              className={styles.input}
+              onChange={this.handelNameChange}
+              value={this.state.name}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label className={styles.formLabel}>
+            {' '}
+            Number
+            <input
+              className={styles.input}
+              onChange={this.handelNumberChange}
+              value={this.state.number}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit"> add contact</button>
+        </form>
+      </>
+    );
+  }
+}
+Form.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+export default Form;
